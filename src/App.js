6b477f117fb037a8b8d06d6fe4a7740bc6bc0479
Jarodwr <wrjarod@gmail.com>
2020-11-19 15:31:40 +1100

    add devtools 1 liner

--- src/App.js
+++ src/App.js
@@ -5,29 +5,27 @@ import { login, signout, requestSignout, cancelSignoutRequest } from './redux/ac
 import './App.css';
 import { getAuthState } from './redux/selectors';
 
-const App = (props) => {
-  return (
-    <div>
-      <AppBar
-        title="Title"
-        iconElementRight={
-          props.loggedIn
-            ? <FlatButton label="Sign out" onClick={props.requestSignout} />
-            : <FlatButton label="Login" onClick={props.login} />}
-      />
-      <Dialog
-        title="Are you sure you would like to sign out?"
-        actions={[
-          <FlatButton label="Yes" onClick={props.signout} />,
-          <FlatButton label="No" onClick={props.cancelSignoutRequest} />
-        ]}
-        model={true}
-        open={props.showSignoutConfirmationDialog}
-      >
-      </Dialog>
-    </div>
-  );
-}
+const App = props => (
+  <div>
+    <AppBar
+      title="Title"
+      iconElementRight={
+        props.loggedIn
+          ? <FlatButton label="Sign out" onClick={props.requestSignout} />
+          : <FlatButton label="Login" onClick={props.login} />}
+    />
+    <Dialog
+      title="Are you sure you would like to sign out?"
+      actions={[
+        <FlatButton label="Yes" onClick={props.signout} />,
+        <FlatButton label="No" onClick={props.cancelSignoutRequest} />
+      ]}
+      model={true}
+      open={props.showSignoutConfirmationDialog}
+    >
+    </Dialog>
+  </div>
+);
 
 // export default App;
 export default connect(getAuthState, { login, signout, requestSignout, cancelSignoutRequest })(App)
